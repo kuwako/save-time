01e007f6625d059bc73302fece476d28eae002a8
masaki_kuwako <m.kuwako0702@gmail.com>
2015-09-20 02:25:54 +0900

    テーブルビューに表示までできた。

---
+++ save-time/ApplicationData.swift
@@ -0,0 +1,25 @@
+//
+//  ApplicationData.swift
+//  save-time
+//
+//  Created by masaki_kuwako on 2015/09/20.
+//  Copyright © 2015年 masaki_kuwako. All rights reserved.
+//
+
+import Foundation
+
+class ApplicationData: NSObject{
+    var time: NSDate = NSDate()
+    var appName: String = "application name"
+    var isActive :Bool = true
+
+    func getTimeStr() -> String {
+        let jaLocale = NSLocale(localeIdentifier: "ja_JP")
+        let fmt = NSDateFormatter()
+        fmt.locale = jaLocale
+        fmt.timeStyle = .LongStyle
+        fmt.dateStyle = .LongStyle
+        
+        return fmt.stringFromDate(self.time)
+    }
+}
--- save-time/ViewController.swift
+++ save-time/ViewController.swift
@@ -10,10 +10,39 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    var appArr : [ApplicationData] = []
+    
+    @IBOutlet weak var timeTable: NSTableView!
+    @IBAction func saveButton(sender: AnyObject) {
+        
+        let app = NSApplication.sharedApplication()
+        print(app.mainWindow!.title)
+        
+        let appObj = ApplicationData()
+        appObj.appName = app.mainWindow!.title
+        
+        appArr.append(appObj)
+
+        timeTable.reloadData()
+        
+//        for obj in winArr {
+//            NSLog("window = %@",obj.title)
+//        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Initialize appArr
+        print("Initialize App")
+        print(appArr.count)
+        if self.appArr.count == 0 {
+            var appData : ApplicationData!
+            
+            appData = ApplicationData()
+            appData.appName="masaki"
+            appArr.append(appData)
+        }
     }
 
     override var representedObject: AnyObject? {
@@ -21,7 +50,28 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    func numberOfRowsInTableView(timeTable: NSTableView) -> Int
+    {
+        return appArr.count
+    }
+    
+    func tableView(timeTable: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+        let timeStr = appArr[row].getTimeStr()
+        let appName = appArr[row].appName
+        let columnName = tableColumn?.identifier
+        
+        if columnName == "time" {
+            return timeStr
+        }
+        else if columnName == "application" {
+            return appName
+        }
+        else if columnName == "active" {
+            return "true"
+        }
+        
+        return ""
+    }
 }
 
